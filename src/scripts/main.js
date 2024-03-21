369115ca92a239f0157367e39163b26c7b733442
Matheus Santos de Santana <134753986+maatheusantanadev@users.noreply.github.com>
2024-03-21 01:10:07 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
-document.querySelector(".aqui").addEventListener("click", function (){
+document.querySelector(".link6").addEventListener("click", function (){
     alert("Estimular a participação ativa da população nas decisões que afetam a comunidade, promovendo a transparência, a responsabilidade compartilhada e o comprometimento coletivo com a sustentabilidade ambiental. Nosso compromisso é com um plano realista, que coloque em prática ações essenciais para a melhoria da cidade. Entendemos que é tempo de uma nova abordagem, um vereador que combine experiência com inovação, e que esteja verdadeiramente alinhada com as necessidades da população. Nós não estamos aqui apenas para criticar, mas para oferecer uma alternativa concreta e viável. Com a experiência e conhecimento de Joelton Lopes, buscando assim  fiscalizar e exigir por transparência e eficácia, ele está mais do que preparado para assumir essa função, ser a voz do povo e promover uma era de mudanças significativas e positivas para nossa cidade. Juntos, faremos muito mais; vamos inspirar e realizar a mudança que desejamos ver, melhorando a vida de cada cidadão. Vamos nessa, de mãos dadas, construir o futuro que nossa cidade.");
+});
+
+
+document.querySelector(".link5").addEventListener("click", function (){
+    alert("Priorizar a educação como alicerce para o desenvolvimento humano, econômico e social, incluindo a conscientização e educação ambiental como pilares para a formação de cidadãos responsáveis e engajados.");
+});
+
+
+document.querySelector(".link4").addEventListener("click", function (){
+    alert("Compromisso com um desenvolvimento que seja economicamente viável, socialmente justo e ambientalmente responsável, assegurando o bem-estar das presentes e futuras gerações.");
+});
+
+document.querySelector(".link3").addEventListener("click", function (){
+    alert("Garantiremos que nenhuma parte de nossa comunidade seja esquecida, promovendo políticas que ofereçam igualdade de oportunidades para todos.");
+});
+
+document.querySelector(".link2").addEventListener("click", function (){
+    alert("Queremos ir além do básico, assegurando que todos os serviços prestados sejam de alta qualidade, realmente fazendo a diferença na vida das pessoas.");
+});
+
+document.querySelector(".link1").addEventListener("click", function (){
+    alert("Comprometemo-nos a ser vereador aberto, mostrando claramente como cada real do dinheiro público é utilizado.");
 });
